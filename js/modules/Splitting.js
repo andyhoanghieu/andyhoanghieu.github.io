f13ac678587df80ba8e96765cefaf8c9c467fdbb
andyhoanghieu <andyhoanghieu@gmail.com>
2025-05-26 07:46:54 +0700

    Delete js directory

--- js/modules/Splitting.js
+++
@@ -1,3 +0,0 @@
-export default function SplittingFunction() {
-    Splitting({ by: "chars" });
-  }  
--- js/services/index.js
+++
@@ -1,7 +0,0 @@
-function lerp(a, b, n) {
-    return (1 - n) * a + n * b;
-  }
-  
-  export {
-    lerp
-  }
